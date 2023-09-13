97ca3877def0711fc12c27ddbcbb075a0483a6dc
Davide <davidemorandini98@gmail.com>
2023-09-13 17:22:52 +0200

    todolist dev 3

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,19 +3,54 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            inputValue: "",
             todoList: [
-                // {done: false},
+                {
+                    text: "Preparare la postazione di lavoro",
+                    done: false,
+                },
+                {
+                    text: "Aprire un ticket in caso di necessità",
+                    done: false,
+                },
+                {
+                    text: "Leggere la documentazione",
+                    done: false,
+                },
+                {
+                    text: "Sperare che tutto vada bene altrimenti...",
+                    done: false,
+                },
             ],
-            inputText: "",
         }
     },
 
     methods: {
-        addText() {
-            this.todoList.push(this.inputText)
+        doneItem(justDone) {
+            justDone.done = !justDone.done;
+        },
+
+        addItem() {
+            if (this.todoList.length > 0) {
+                this.todoList.push({text:this.inputValue, done: false})
+                this.inputValue = "";
+            }
+        },
+
+        deleteItem(canceledIndex) {
+            this.todoList.splice(canceledIndex, 1)
         }
-    }
-
-
-    
+    } 
 }).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
